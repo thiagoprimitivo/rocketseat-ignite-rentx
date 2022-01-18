d175efe2bce96d8f2c16f96138e142b99025ab99
Thiago Primitivo de Oliveira <thiago.primitivo@gmail.com>
2022-01-18 06:18:02 -0300

    Adicionando coluna de avatar

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -35,6 +35,10 @@ export async function ensureAuthenticated(
       throw new AppError('User not found!', 401);
     }
 
+    request.user = {
+      id: user_id,
+    };
+
     next();
   } catch {
     throw new AppError('Invalid token!', 401);
