a47a72d3986c88d40278643540edca23a782e4b0
Thiago Primitivo de Oliveira <thiago.primitivo@gmail.com>
2022-03-01 06:40:19 -0300

    Criando teste de criação do carro

---
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -0,0 +1,17 @@
+class Car {
+  name: string;
+
+  description: string;
+
+  daily_rate: number;
+
+  license_plate: string;
+
+  fine_amount: number;
+
+  brand: string;
+
+  category_id: string;
+}
+
+export { Car };
