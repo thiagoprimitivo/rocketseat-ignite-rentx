fdee15600082bb529368fcddfa278d57c739634f
Thiago Primitivo de Oliveira <thiago.primitivo@gmail.com>
2022-04-26 10:46:43 -0300

    Criando seed de usuário

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,7 +2,7 @@ import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
 
-import '@shared/infra/typeorm';
+import createConnection from '@shared/infra/typeorm';
 
 import '@shared/container';
 
@@ -10,6 +10,8 @@ import { AppError } from '@shared/errors/AppError';
 
 import swaggerFile from '../../../swagger.json';
 import { router } from './routes';
+
+createConnection();
 
 const app = express();
 
